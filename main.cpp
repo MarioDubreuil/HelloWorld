09e901fc38286e2832965a973842c39dddf5071e
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-11-27 21:24:12 -0500

    naming convention

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 
+//void hello() {
+//    std::cout << "Hello world!\n";
+//}
+
+//void exchange() {
+//    const double pi = 3.14;
+//    double r = 10;
+//    double c = 2 * pi * r;
+//    std::cout << "pi = " << pi << "\n";
+//    std::cout << "r = " << r << "\n";
+//    std::cout << "c = 2*pi*r = " << c << "\n";
+//}
+
+void namingConvention() {
+    int file_size;  // snake case
+    int FileSize;   // pascal case
+    int fileSize;   // camel case
+}
+
 int main() {
-    const double pi = 3.14;
-    double r = 10;
-    double c = 2 * pi * r;
-    std::cout << "pi = " << pi << "\n";
-    std::cout << "r = " << r << "\n";
-    std::cout << "c = 2*pi*r = " << c << "\n";
+//    hello();
+//    exchange();
     return 0;
 }
